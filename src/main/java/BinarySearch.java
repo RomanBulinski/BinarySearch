c8e9cf9be8bb9d7e78b590010001eee4dd6dff77
Roman Bulinski <aboandrus@gmail.com>
2019-03-03 20:46:25 +0100

    Implemented code with tests

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -1,17 +1,26 @@
 
 public class BinarySearch {
 
+    public boolean binarySearch(  int[] arr, int number  ){
 
-    public boolean binarySearch(  int arr[], int number  ){
+            int start =0;
+            int end = arr.length-1;
 
-
-        
-
-
+        while (start+1 < end) {
+            int midle = (end-start) /2 + start;
+            if( arr[midle]==number){
+                return true;
+            }else{
+                if( number < arr[midle] ){
+                    end = midle;
+                }
+                if( number > arr[midle] ){
+                    start = midle;
+                }
+            }
+        }
+        return false;
     }
-
-
-
 
 
 }
--- src/test/java/TestBinarySearch.java
+++ src/test/java/TestBinarySearch.java
@@ -1,11 +1,46 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestBinarySearch {
 
-    @TestTemplate
-    public  void testBinarySearch(){
-
-
-
+    @Test
+    public  void testBinarySearchTRue(){
+        int[] arr = {1, 2, 3, 4};
+        BinarySearch bs = new BinarySearch();
+        assertTrue(  bs.binarySearch( arr,3    )   );
     }
+
+    @Test
+    public  void testBinarySearchTRue2(){
+        int[] arr = {1, 2, 3, 4, 5 };
+        BinarySearch bs = new BinarySearch();
+        assertTrue(  bs.binarySearch( arr,4   )   );
+    }
+
+    @Test
+    public  void testBinarySearchFalse(){
+        int[] arr = {11,12,13,15,16};
+        BinarySearch bs = new BinarySearch();
+        assertFalse(  bs.binarySearch( arr,14   )   );
+    }
+
+    @Test
+    public  void testBinarySearchFalse2(){
+        int[] arr = {9,10,11,12,13,15,16,17};
+        BinarySearch bs = new BinarySearch();
+        assertFalse(  bs.binarySearch( arr,14   )   );
+    }
+
+    @Test
+    public  void testBinarySearchFalse3(){
+        int[] arr = {9,10,11};
+        BinarySearch bs = new BinarySearch();
+        assertFalse(  bs.binarySearch( arr,20  )   );
+    }
+
+
+
 }
